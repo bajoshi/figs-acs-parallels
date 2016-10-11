4d47337f8506fb019fa820e3085bd30ebb72c6b2
Bhavin Joshi <bajoshi@asu.edu>
2016-10-11 11:27:04 -0700

    updates to movefiles.py

--- codes/movefiles.py
+++ codes/movefiles.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     imdriz = home + '/acspar/IMDRIZZLE' + field + posang + '/'
     savdir = os.getenv('AXE_SAVEDIR_PATH') + '/'
-    datadir = "/Volumes/Bhavins_backup/acs_parallels_data/flc/"
+    flcdir = "/Volumes/Bhavins_backup/acs_parallels_data/flc/"
     listdir = acs_home
     
     ##############
@@ -23,31 +23,60 @@ if __name__ == '__main__':
     
     # --------------------------------------------
     # This is for the direct image files list
-    
     filt = 'f814w'
     
     fh = open(listdir + filt + field + posang + '.lis', 'r')
+
+    print "Will copy the following direct image files to IMDRIZZLE and SAVE"
+    print fh.read().splitlines()
     
     for file in fh.read().splitlines():
-        print "Copying direct image", file, "from FLC directory to IMDRIZZLE and SAVE"
-        shutil.copy(datadir + file, imdriz + file) # source is first arg and destination is second
-        shutil.copy(datadir + file, savdir + file)
+        
+        # Check if file exists and if it doesn't then copy it.
+        if os.path.isfile(savdir + file):
+            print file, "exists in SAVE. Checking IMDRIZZLE next."
+        elif not os.path.isfile(savdir + file):
+            print "Copying grism image", file, "from FLC directory to SAVE"
+            shutil.copy(flcdir + file, savdir + file)  # source is first arg and destination is second
+
+        if os.path.isfile(imdriz + file):
+            print file, "exists in IMDRIZZLE. Moving to next file in the list."
+        elif not os.path.isfile(imdriz + file):
+            print "Copying direct image", file, "from FLC directory to IMDRIZZLE"
+            shutil.copy(flcdir + file, imdriz + file)
     
     shutil.copy(listdir + filt + field + posang + '.lis', imdriz + filt + '.lis')
     print "\n"
-    # this second copy line is required because the combined drizzled image will be made in the IMDRIZZLE folder
+    # this second copy line is required because the combined drizzled direct image will be made in the IMDRIZZLE folder
     
     # --------------------------------------------
     # This is for the grism image files list
-    # These need to be copied to SAVE but not to IMDRIZZLE. IMDRIZZLE only needs the direct image files
     
     filt = 'g800l'
     
-    fh = open(listdir + filt + field + posang + '.lis', 'r')
+    fh = open(listdir + filt + '_f814w' + field + posang + '.lis', 'r')
     
+    print "Will copy the following grism image files to IMDRIZZLE and SAVE"
+    print fh.read().splitlines()
+
     for file in fh.read().splitlines():
-        print "Copying grism image", file, "from FLC directory to SAVE"
-        shutil.copy(datadir + file, savdir + file)
+
+        # Check if file exists and if it doesn't then copy it.
+        if os.path.isfile(savdir + file):
+            print file, "exists in SAVE. Checking IMDRIZZLE next."
+        elif not os.path.isfile(savdir + file):
+            print "Copying grism image", file, "from FLC directory to SAVE"
+            shutil.copy(flcdir + file, savdir + file)
+
+        if os.path.isfile(imdriz + file):
+            print file, "exists in IMDRIZZLE. Moving to next file in the list."
+        elif not os.path.isfile(imdriz + file):
+            print "Copying grism image", file, "from FLC directory to IMDRIZZLE"
+            shutil.copy(flcdir + file, imdriz + file)
+
+    shutil.copy(listdir + filt + field + posang + '.lis', imdriz + filt + '.lis')
+    print "\n"
+    # this second copy line is required because the combined drizzled grism image will be made in the IMDRIZZLE folder
     
     sys.exit(0)
 
